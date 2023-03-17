4d8be71f82b59ed4f5c1ebf7bf0c674c9b10e81e
RobertFranczak <94225378+RobertFranczak@users.noreply.github.com>
2023-03-17 16:50:42 +0100

    Added Messages layout

---
+++ RLTest/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  RLTest
+//
+//  Created by Robert Franczak on 16/03/2023.
+//
+
+
+struct Constants {
+    static let registerSegue = "RegisterToSuccess"
+    static let loginSegue    = "LoginToSuccess"
+    static let appName       = "📱MyTest App"
+    static let cellID        = "ReusableCell"
+    static let cellNibname   = "MessageCell"
+}
--- RLTest/Controler/SuccessViewController.swift
+++ RLTest/Controler/SuccessViewController.swift
@@ -6,12 +6,71 @@
 //
 
 import UIKit
-
+import Firebase
 class SuccessViewController: UIViewController {
     
+    @IBOutlet weak var messageTextfield: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var messages: [Message] = [
+        Message(sender: "1@2.com", body: "Hi!"),
+        Message(sender: "a@b.com", body: "Hello!"),
+        Message(sender: "1@2.com", body: "What's up?!")
+    ]
+    
     override func viewDidLoad() {
+        //tableView.delegate = self
+        tableView.dataSource = self
+        title = Constants.appName
         super.viewDidLoad()
-
+        navigationItem.hidesBackButton = true
+        
+        tableView.register(UINib(nibName: Constants.cellNibname, bundle: nil), forCellReuseIdentifier: Constants.cellID)
+    }
+    
+    
+    @IBAction func sendMessagePressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
+        let firebaseAuth = Auth.auth()
+        do {
+          try firebaseAuth.signOut()
+            navigationController?.popToRootViewController(animated: true)
+        } catch let signOutError as NSError {
+          print("Error signing out: %@", signOutError)
+        }
     }
 }
+
+extension SuccessViewController : UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return messages.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        // create cell
+        let cell = tableView.dequeueReusableCell(withIdentifier: Constants.cellID, for: indexPath) as! MessageCell
+        
+        cell.label.text = "\(messages[indexPath.row].body)"
+    
+        // config this cell
+//        var config = UIListContentConfiguration.cell()
+//        config.text = "\(messages[indexPath.row].sender)"
+//        config.secondaryText = "\(messages[indexPath.row].body)"
+//        config.image = UIImage(named: "MeAvatar")
+//        cell.contentConfiguration = config
+        
+        
+        return cell
+    }
+}
+
+//extension SuccessViewController : UITableViewDelegate {
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//        print(indexPath.row)
+//    }
+//}
 
